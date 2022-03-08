425acdc731d3191ff24e11dadddeff9dca9cee13
Memexurer <{meme}+xurer@testzeber.pl>
2022-03-08 12:33:45 +0100

    initial cummit

---
+++ Dupa.js
@@ -0,0 +1,43 @@
+Ra/// api_version=2
+var script = registerScript({
+    name: "Dupa",
+    version: "1",
+    authors: ["Rurkowanie"]
+});
+
+script.registerModule({
+  name: "Shop",
+  description: "Otwiera menu villagerowe ze sklepem", 
+  category: "Misc"
+}, function(module) {
+  module.triggerType = Java.type("net.ccbluex.liquidbounce.features.module.EnumTriggerType").PRESS;
+  
+  module.on("enable", function(){
+    for(var id in mc.theWorld.getLoadedEntityList()) {
+      var entity = mc.theWorld.getLoadedEntityList()[id];
+      if(entity.class.getName().contains("Villager")) {
+        mc.playerController.interactWithEntitySendPacket(mc.thePlayer, entity)
+        return;
+      }
+    }
+  });
+});
+
+script.registerModule({
+  name: "Upgradess",
+  description: "Otwiera menu villagerowe z ulepszeniami", 
+  category: "Misc"
+}, function(module) {
+  module.triggerType = Java.type("net.ccbluex.liquidbounce.features.module.EnumTriggerType").PRESS;
+  
+  module.on("enable", function(){
+    var foundNumber = 0;
+    for(var id in mc.theWorld.getLoadedEntityList()) {
+      var entity = mc.theWorld.getLoadedEntityList()[id];
+      if(entity.class.getName().contains("Villager") && foundNumber++ >= 1) {
+        mc.playerController.interactWithEntitySendPacket(mc.thePlayer, entity)
+        return;
+      }
+    }    
+  });
+});
---
+++ Dupa2.js
@@ -0,0 +1,37 @@
+/// api_version=2
+var script = registerScript({
+    name: "Dupa2",
+    version: "1",
+    authors: ["Rurkowanie"]
+});
+
+var MovementUtils = Java.type("net.ccbluex.liquidbounce.utils.MovementUtils").INSTANCE;
+var KillAura = Java.type("net.ccbluex.liquidbounce.features.module.modules.combat.KillAura");
+
+script.registerModule({
+  name: "Speed",
+  description: "Aha", 
+  category: "Movement"
+}, function(module) {
+  var sexo = 0;
+  
+  module.on("update", function(event){
+    if(!MovementUtils.isMoving()) {
+      sexo = 0;
+      return;
+    }
+      
+    if (mc.thePlayer.isOnLadder() || mc.thePlayer.isEating() || mc.thePlayer.isInLava())
+      return;
+  
+    if (mc.thePlayer.onGround) {
+      mc.thePlayer.jump();
+    } else if(mc.thePlayer.motionY > 0){
+      mc.thePlayer.motionY = (sexo <= -0.2 ? sexo = 0 : sexo -= 0.01);
+    }
+  });
+  
+  module.on("disable", function(){
+    sexo = 0;
+  });
+});
